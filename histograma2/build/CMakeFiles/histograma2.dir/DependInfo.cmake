b1a1ba807534b898db9ce64e3e09a84e1f416069
Jaquelin Paz Paz <Jaquelin.Paz@hotmail.com>
2016-01-21 17:44:39 -0600

    Comentario

---
+++ histograma2/build/CMakeFiles/histograma2.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/motoman_yaskawa/Desktop/2016/histograma2/histograma2.cpp" "/home/motoman_yaskawa/Desktop/2016/histograma2/build/CMakeFiles/histograma2.dir/histograma2.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ histograma2/build/CMakeFiles/histograma2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/histograma2.dir/histograma2.cpp.o"
+  "histograma2.pdb"
+  "histograma2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/histograma2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
